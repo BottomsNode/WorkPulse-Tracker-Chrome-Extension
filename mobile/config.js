e3b725d4f0b91b2857db7541a1f138828fb7865d
nishit <nishitshiv2001@gmail.com>
2025-09-10 15:29:59 +0530

    removed the mobile folder

--- mobile/config.js
+++
@@ -1,7 +0,0 @@
-const DEFAULT_HRMS = {
-    hrmsApi: "https://hrms-api.taskgrids.com/api/attendance/generateListings",
-
-    hrmsUser: { firstname: "Nishit", id: 928, employee_id: "25DEV010" },
-
-    hrmsToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vaHJtcy1hcGkudGFza2dyaWRzLmNvbS9hcGkvbG9naW4iLCJpYXQiOjE3NTYxMTYyMTIsImV4cCI6MTc1ODcwODIxMiwibmJmIjoxNzU2MTE2MjEyLCJqdGkiOiJENEZPVXJudThyRjJjMGdVIiwic3ViIjoiOTI4IiwicHJ2IjoiZjkzMDdlYjVmMjljNzJhOTBkYmFhZWYwZTI2ZjAyNjJlZGU4NmY1NSJ9.VLxhdxrXldQzwT3hzy9c_kzK_UoMJl597XKNHbs27R8",
-};
